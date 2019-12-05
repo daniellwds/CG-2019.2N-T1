f1f1e1fc07097af8c37c7a514ce7ef46c014a182
Daniel Welter <danielluiswelter@gmail.com>
2019-12-05 00:05:36 -0300

    Add movement to Mimikyu

--- script.js
+++ script.js
@@ -45,7 +45,10 @@
             scene.add(light.target);
         }
 
-        let mimikyu; 
+        let mimikyu, head, 
+        earRight, earTipRight, earLeft,
+        earTipLeft, tail; 
+        
         {
             const gltfLoader = new GLTFLoader();
             gltfLoader.load(
@@ -55,15 +58,68 @@
                     scene.add(root);
 
                     root.scale.set(2.5, 2.5, 2.5)
+                    
+                    head = root.getObjectByName('Head_Armature')
+                    earRight = root.getObjectByName('EarR_Armature')
+                    earTipRight = root.getObjectByName('Ear_tipR_Armature')
+                    earLeft = root.getObjectByName('EarL_Armature')
+                    earTipLeft = root.getObjectByName('Ear_tipL_Armature')
+                    tail = root.getObjectByName('Tail_Armature')
                 });
         }
 
-        function render(time) {
+        let unitMovementEarRight = -1,
+        unitMovementEarLeft = -1,
+        unitMovementEarTipRight = -1,
+        unitMovementEarTipLeft = -1,
+        unitMovementTail = -1,
+        unitMovementHead = -1
+
+        function animate() {    
+            if (mimikyu) {          
+
+                if (tail.rotation.y > 0.9 || tail.rotation.y < -0.7){                    
+                    unitMovementTail = unitMovementTail * -1
+                }
+
+                if (head.rotation.z > 0.1 || head.rotation.z < -0.5){
+                    unitMovementHead = unitMovementHead * -1
+                }
+                
+                if (earLeft.rotation.z > 0.1 || earLeft.rotation.z < -0.5){
+                    unitMovementEarLeft = unitMovementEarLeft * -1
+                }
+
+                if (earRight.rotation.z > 0.1 || earRight.rotation.z < -0.5){
+                    unitMovementEarRight = unitMovementEarRight * -1
+                }
+
+                if (earTipLeft.rotation.z > 0.1 || earTipLeft.rotation.z < -0.5){
+                    unitMovementEarTipLeft = unitMovementEarTipLeft * -1
+                }
+
+                if (earTipRight.rotation.z > 0.1 || earTipRight.rotation.z < -0.5){
+                    unitMovementEarTipRight = unitMovementEarTipRight * -1
+                }
+
+                head.rotation.z += 0.005 * unitMovementHead
+                earLeft.rotation.z += 0.01 * unitMovementEarLeft
+                earRight.rotation.z += 0.01 * unitMovementEarRight
+                earTipLeft.rotation.z += 0.01 * unitMovementEarTipLeft
+                earTipRight.rotation.z += 0.01 * unitMovementEarTipRight
+                tail.rotation.y += 0.01 * unitMovementTail
+                tail.rotation.z += 0.01 * unitMovementTail
+            }
+        }
+
+        function render() {
             renderer.render(scene, camera);
             requestAnimationFrame(render);
+            animate()
         }
 
         requestAnimationFrame(render);
     }
 
     main()
+    
